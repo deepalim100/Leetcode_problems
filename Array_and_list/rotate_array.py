44c5cf802811410d5a10ff2794a1b56f43e88273
deepalim100 <deepsiya10@gmail.com>
2024-06-20 22:23:08 +0530

    rotate array by right side

---
+++ Array_and_list/rotate_array.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/rotate-array/description/
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        if k > 0:
+            nums[:k], nums[k:] = nums[-k:], nums[:-k]
+        return nums
